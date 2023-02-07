e0c28c01ed1e3cc218a924aa3268ad2e0e8fd33e
annadu <duyuan0921@gmail.com>
2023-02-07 09:48:33 -0800

    add header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import Card from './components/cards/Card'
+import NewCard from './components/cards/NewCard'
+import CardList from './components/cards/CardList'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>CheerUp Inspiration Board</h1>
         
-        <p>Learn React</p>
+        
         <Card/>
+        <NewCard/>
+        
       </header>
     </div>
   );
