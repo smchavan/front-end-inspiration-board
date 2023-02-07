18acd8b5fb9f26696a579e0c0d012999027656f2
annadu <duyuan0921@gmail.com>
2023-02-07 09:27:10 -0800

    delete useState

--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -2,23 +2,36 @@ import React from 'react';
 import PropTypes from "prop-types"
 import "./Card.css";
 
-const Card = ({ id, message, likes, deleteCard, likeCard }) =>{
-  return (
-    <div>
-      <h4>{message}</h4>
-      <span>{likes}💕</span>
-      <button onClick={() => likeCard(id)}>+1</button>
-      <button onClick={() => deleteCard(id)}>Delete</button>
+const Card = (props) =>{
+  const onLikeButtonClick = () => {
+    console.log(props.card_id);
+    props.likeCard(props.card_id);
+  };
 
+  const onDeleteButtonClick =() =>{
+    console.log(props.card_id);
+    props.deleteCard(props.card_id);
+  };
+
+  return (
+    <div className="card">
+      <p className='card_message'>{props.message}</p>
+      <section className='cards_buttons'>
+        <p>{props.likes_count}💕</p>
+      
+        <button  className='like_button' type='button' onClick={onLikeButtonClick}>+1</button>
+        <button  className='delete_button' type='button'onClick={onDeleteButtonClick}>Delete</button>
+      </section>
     </div>
-  )
-}
+  );
+};
 
 Card.propTypes = {
-  id: PropTypes.number.isRequired,
+  card_id: PropTypes.number.isRequired,
   message: PropTypes.string.isRequired,
-  likes: PropTypes.number,
+  likes_count: PropTypes.number,
   deleteCard: PropTypes.func.isRequired,
   likeCard: PropTypes.func.isRequired,
 };
+
 export default Card;
