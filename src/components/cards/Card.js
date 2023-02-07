5ace9465fb2bd1b300c1658c6239e8f0114afd2b
annadu <duyuan0921@gmail.com>
2023-02-06 22:02:59 -0800

    add +1 and delete button

--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -2,11 +2,23 @@ import React from 'react';
 import PropTypes from "prop-types"
 import "./Card.css";
 
-const Card = (props) =>{
+const Card = ({ id, message, likes, deleteCard, likeCard }) =>{
   return (
     <div>
+      <h4>{message}</h4>
+      <span>{likes}💕</span>
+      <button onClick={() => likeCard(id)}>+1</button>
       <button onClick={() => deleteCard(id)}>Delete</button>
 
     </div>
   )
 }
+
+Card.propTypes = {
+  id: PropTypes.number.isRequired,
+  message: PropTypes.string.isRequired,
+  likes: PropTypes.number,
+  deleteCard: PropTypes.func.isRequired,
+  likeCard: PropTypes.func.isRequired,
+};
+export default Card;
