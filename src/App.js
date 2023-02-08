68acc3ec7548e4723a86e2bcfc124687d4cde8d5
annadu <duyuan0921@gmail.com>
2023-02-08 09:25:29 -0800

    test likeCard done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React,{useState, useEffect} from 'react';
-import './App.css';
-import Card from './components/cards/Card'
-import NewCard from './components/cards/NewCard'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Card from "./components/cards/Card";
+import NewCard from "./components/cards/NewCard";
 //import CardList from './components/cards/CardList'
 import axios from "axios";
+import CardList from "./components/cards/CardList";
 
 function App() {
-  const [cardsData, setCardsData] = useState([]);
+  const [cardsData, setCardsData] = useState([
+    {
+      board_id: 1,
+      board_title: "Legos are Fun!",
+      id: 1,
+      likes_count: 3,
+      message: "Lego Lego Lego Fun",
+    },
+    {
+      board_id: 1,
+      board_title: "Legos are Fun!",
+      id: 2,
+      likes_count: 4,
+      message: "Puzzles are also Fun like Legos",
+    },
+  ]);
 
-  const getAllCards  = (boardId) => {
+  const getAllCards = (boardId) => {
     axios
       .get(`${process.env.REACT_APP_BACKEND_URL}/boards/${boardId}/cards`)
       .then((response) => {
@@ -19,18 +35,30 @@ function App() {
         console.error(error.response.data.message);
       });
   };
-  
-  
-  
+  const likeCard = () => {
+    const cards = cardsData.map((card) => {
+      if (card.id === 2) {
+        card.likes_count += 1;
+        return card;
+      } else {
+        return card;
+      }
+
+    });
+
+    setCardsData(cards);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>CheerUp Inspiration Board</h1>
-      </header>  
-      <body> 
-        <Card/>
-        <NewCard/>
-      </body> 
+      </header>
+      <body>
+        <CardList cardsData={cardsData} likeCard={likeCard} />
+
+        <NewCard />
+      </body>
       <footer>
         <p4>Made by Anna, Larissa, Melody, Supriya </p4>
       </footer>
