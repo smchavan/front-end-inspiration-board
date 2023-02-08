7dfb7b75b2c4ea2e1cb9f4732522c1fdd503b21d
larissa <larza.ault@gmail.com>
2023-02-08 10:06:53 -0800

    Initialized boardsdataset collection - still working on controlling the API call for this initialization

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React,{useState, useEffect} from 'react';
-import './App.css';
-import Card from './components/cards/Card'
-import NewCard from './components/cards/NewCard'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Card from "./components/cards/Card";
+import NewCard from "./components/cards/NewCard";
+import BoardsList from "./components/boards/BoardsList";
 //import CardList from './components/cards/CardList'
 import axios from "axios";
 
 function App() {
+  // create states for boards and cards data
   const [cardsData, setCardsData] = useState([]);
+  const [boardsData, setBoardsData] = useState([]);
 
-  const getAllCards  = (boardId) => {
+  // Function to display card data
+  const getAllCards = (boardId) => {
     axios
       .get(`${process.env.REACT_APP_BACKEND_URL}/boards/${boardId}/cards`)
       .then((response) => {
@@ -19,18 +23,34 @@ function App() {
         console.error(error.response.data.message);
       });
   };
-  
-  
-  
+
+  // Function to display board data
+  const getAllBoards = () => {
+    axios
+      .get(`${process.env.REACT_APP_BACKEND_URL}/boards`)
+      .then((response) => {
+        console.log(response.data);
+        setBoardsData(response.data);
+      })
+      .catch((error) => {
+        console.error(error.response.data.message);
+      });
+  };
+  getAllBoards();
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>CheerUp Inspiration Board</h1>
-      </header>  
-      <body> 
-        <Card/>
-        <NewCard/>
-      </body> 
+      </header>
+      <BoardsList
+        boardsData={boardsData}
+        getAllBoards={getAllBoards}
+      ></BoardsList>
+      <body>
+        <Card />
+        <NewCard />
+      </body>
       <footer>
         <p4>Made by Anna, Larissa, Melody, Supriya </p4>
       </footer>
