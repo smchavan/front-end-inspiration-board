c063bc41a60833baca61b5d4f3673a5f70c5cd3e
larissa <larza.ault@gmail.com>
2023-02-08 10:38:56 -0800

    Fixed infinite API calls to only render once upon mounting boards data set

--- src/App.js
+++ src/App.js
@@ -11,6 +11,19 @@ function App() {
   const [cardsData, setCardsData] = useState([]);
   const [boardsData, setBoardsData] = useState([]);
 
+  // initialize boards dataset upon mounting
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_BACKEND_URL}/boards`)
+      .then((response) => {
+        console.log(response.data);
+        setBoardsData(response.data);
+      })
+      .catch((error) => {
+        console.error(error.response.data.message);
+      });
+  }, []);
+
   // Function to display card data
   const getAllCards = (boardId) => {
     axios
@@ -24,37 +37,18 @@ function App() {
       });
   };
 
-  // Function to display board data
-  const getAllBoards = () => {
-    axios
-      .get(`${process.env.REACT_APP_BACKEND_URL}/boards`)
-      .then((response) => {
-        console.log(response.data);
-        setBoardsData(response.data);
-      })
-      .catch((error) => {
-        console.error(error.response.data.message);
-      });
-  };
-  getAllBoards();
-
   return (
-    <div className="App">
+    <main className="App">
       <header className="App-header">
         <h1>CheerUp Inspiration Board</h1>
       </header>
-      <BoardsList
-        boardsData={boardsData}
-        getAllBoards={getAllBoards}
-      ></BoardsList>
-      <body>
-        <Card />
-        <NewCard />
-      </body>
+      <BoardsList boardsData={boardsData}></BoardsList>
+      <Card />
+      <NewCard />
       <footer>
-        <p4>Made by Anna, Larissa, Melody, Supriya </p4>
+        <h4>Made by Anna, Larissa, Melody, Supriya </h4>
       </footer>
-    </div>
+    </main>
   );
 }
 
