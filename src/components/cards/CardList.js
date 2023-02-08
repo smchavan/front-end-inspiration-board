1dcda9f1f265dd79e6886426e97f64c332a15b9d
annadu <duyuan0921@gmail.com>
2023-02-07 21:34:23 -0800

    add CardList

--- src/components/cards/CardList.js
+++ src/components/cards/CardList.js
@@ -5,9 +5,21 @@ import './CardList.css';
 
 const CardList = (props) => {
   return (
-    <Card/>
-  )
-  }
+    <div className="card_entries__container">
+      {props.cardsData.map((card) => (
+        <Card
+          key={card.id}
+          card_id={card.id}
+          board_id={card.board_id}
+          likes_count={card.likes_count}
+          message={card.message}
+          likeCard={props.likeCard}
+          deleteCard={props.deleteCard}
+        />
+      ))}
+    </div>
+  );
+};
 
 
 
