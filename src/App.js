c35f64c9279b5565ce18dddd9d0fe25e1afeb3af
annadu <duyuan0921@gmail.com>
2023-02-07 20:28:39 -0800

    import axios add getAllCards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
+import React,{useState, useEffect} from 'react';
 import './App.css';
 import Card from './components/cards/Card'
 import NewCard from './components/cards/NewCard'
-import CardList from './components/cards/CardList'
+//import CardList from './components/cards/CardList'
+import axios from "axios";
 
 function App() {
+  const [cardsData, setCardsData] = useState([]);
+
+  const getAllCards  = (boardId) => {
+    axios
+      .get(`${process.env.REACT_APP_BACKEND_URL}/boards/${boardId}/cards`)
+      .then((response) => {
+        console.log(response.data);
+        setCardsData(response.data.cards);
+      })
+      .catch((error) => {
+        console.error(error.response.data.message);
+      });
+  };
+
+  
+  
   return (
     <div className="App">
       <header className="App-header">
