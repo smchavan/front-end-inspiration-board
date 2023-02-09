f979a51f4dfbdd6e1cb84d75866701602f291beb
annadu <duyuan0921@gmail.com>
2023-02-09 15:08:44 -0800

    change2

--- src/components/boards/SelectedBoard.js
+++ src/components/boards/SelectedBoard.js
@@ -1,7 +1,16 @@
 import PropTypes from "prop-types";
 
 const SelectedBoard = (props) => {
-  return <p>{props.selectedBoardMessage}</p>;
+  //return <p>{props.selectedBoardMessage}</p>;
+  let message = "Select a Board from the Board List!";
+  if (props.selectedBoardID !== 0) {
+    // find the corresponding board to selectedBoardID
+    const selectedBoard = props.boardsData.filter(
+      (board) => board.id === props.selectedBoardID
+    );
+    message = `${selectedBoard[0].title} - ${selectedBoard[0].creator}`;
+  }
+  return <p>{message}</p>;
 };
 
 export default SelectedBoard;
