6ac7acdd81dc38071bc4176a5502d5762c206bd0
annadu <duyuan0921@gmail.com>
2023-02-09 15:09:10 -0800

    change4

--- src/App.js
+++ src/App.js
@@ -9,50 +9,83 @@ import axios from "axios";
 import CardList from "./components/cards/CardList";
 import ShowCard from "./components/cards/ShowCard";
 
+
+// ~~~~~~ Helper Functions ~~~~~~
+// Function for sending get requests
+const getAllBoards = () => {
+  return axios
+    .get(`${process.env.REACT_APP_BACKEND_URL}/boards`)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.error(error.response.data.message);
+    });
+};
+// Function for sending post requests
+const registerNewBoard = (newBoard) => {
+  // create request body
+  const requestBody = {
+    ...newBoard,
+  };
+  console.log(requestBody);
+  // send post request
+  return axios
+    .post(`${process.env.REACT_APP_BACKEND_URL}/boards`, requestBody)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.log(error.response.data.message);
+    });
+};
+
+
 function App() {
 
 
-  const [cardsData, setCardsData] = useState([]);
-      // {
-      //   board_id: 1,
-      //   board_title: "Legos are Fun!",
-      //   id: 1,
-      //   likes_count: 3,
-      //   message: "Lego Lego Lego Fun",
-      // },
-      // {
-      //   board_id: 2,
-      //   board_title: "Legos are Fun!",
-      //   id: 2,
-      //   likes_count: 4,
-      //   message: "Puzzles are also Fun like Legos",
-      // },
-      // {
-      //   board_id: 3,
-      //   board_title: "test delete",
-      //   id: 3,
-      //   likes_count: 4,
-      //   message: "test delete",
-      // },
-      // {
-      //   board_id: 4,
-      //   board_title: "board 4",
-      //   id: 4,
-      //   likes_count: 4,
-      //   message: "board 4",
-      // },
-      // {
-      //   board_id: 1,
-      //   board_title: "Legos are Fun!",
-      //   id: 5,
-      //   likes_count: 4,
-      //   message: "board1",
-      // },
+  const [cardsData, setCardsData] = useState([
+      {
+        board_id: 1,
+        board_title: "Legos are Fun!",
+        id: 1,
+        likes_count: 3,
+        message: "Lego Lego Lego Fun",
+      },
+      {
+        board_id: 2,
+        board_title: "Legos are Fun!",
+        id: 2,
+        likes_count: 4,
+        message: "Puzzles are also Fun like Legos",
+      },
+      {
+        board_id: 3,
+        board_title: "test delete",
+        id: 3,
+        likes_count: 4,
+        message: "test delete",
+      },
+      {
+        board_id: 4,
+        board_title: "board 4",
+        id: 4,
+        likes_count: 4,
+        message: "board 4",
+      },
+      {
+        board_id: 1,
+        board_title: "Legos are Fun!",
+        id: 5,
+        likes_count: 4,
+        message: "board1",
+      },
+    ])
     
   const getBoardCard = async (boardId) =>{
     return axios
             .get(
-            `${process.env.REACT_APP_BACKEND_URL}/boards/${selectedBoard.id}/cards`,
+            `${process.env.REACT_APP_BACKEND_URL}/boards/${selectedBoardID}/cards`,
             {}
           )
     
@@ -63,7 +96,7 @@ function App() {
         };
     
   useEffect(() =>{
-    getBoardCard(selectedBoard.id);
+    getBoardCard(selectedBoardID);
         }, [BoardsDropDown]);
       
   const addCard = (message) => {
@@ -74,6 +107,10 @@ function App() {
     newCardList.push({
         id: nextId,
         message: message,
+        board_id:selectedBoardID,
+        likes_count:0
+
+
       });
     setCardsData(newCardList);
     };
@@ -109,24 +146,57 @@ function App() {
     };
   
   // ~~~~~~ boards data ~~~~~~
+  // const [boardsData, setBoardsData] = useState([]);
+  // // function to initialize boards data upon mounting
+  // useEffect(() => {
+  //   axios
+  //     .get(`${process.env.REACT_APP_BACKEND_URL}/boards`)
+  //     .then((response) => {
+  //       setBoardsData(response.data);
+  //     })
+  //     .catch((error) => {
+  //       console.error(error.response.data.message);
+  //     });
+  // }, []);
+  // // function to update boards data upon form submission
+  // const updateBoardsData = (newBoard) => {
+  //   // duplicate boardsData
+  //   const newBoardsData = [...boardsData];
+  //   // find next valid board id
+  //   const nextID = Math.max(...newBoardsData.map((board) => board.id)) + 1;
+  //   newBoardsData.push({
+  //     id: nextID,
+  //     title: newBoard.title,
+  //     creator: newBoard.creator,
+  //   });
+  //   setBoardsData(newBoardsData);
+  // };
+
+  // // selected boards message
+  // const [selectedBoard, setSelectedBoard] = useState([]);
+  // // function to update selected board message
+  // const updateSelectedBoard = (selectedBoard) => {
+  //   setSelectedBoard(selectedBoard);
+  //   console.log(selectedBoard);
+  //   //console.log(selectedBoard[0])
+  // };
+  // ~~~~~~ boards data ~~~~~~
   const [boardsData, setBoardsData] = useState([]);
-  // function to initialize boards data upon mounting
+  // function to make get request whenever boardsData gets modified
   useEffect(() => {
-    axios
-      .get(`${process.env.REACT_APP_BACKEND_URL}/boards`)
-      .then((response) => {
-        setBoardsData(response.data);
-      })
-      .catch((error) => {
-        console.error(error.response.data.message);
-      });
+    getAllBoards().then((boards) => {
+      setBoardsData(boards);
+    });
   }, []);
-  // function to update boards data upon form submission
+  // function to update boards data state upon submission
   const updateBoardsData = (newBoard) => {
-    // duplicate boardsData
+    // make a post request to the backend
+    registerNewBoard(newBoard);
+    // change the boardsData state
     const newBoardsData = [...boardsData];
     // find next valid board id
     const nextID = Math.max(...newBoardsData.map((board) => board.id)) + 1;
+    // update the boards data state with new board
     newBoardsData.push({
       id: nextID,
       title: newBoard.title,
@@ -135,13 +205,11 @@ function App() {
     setBoardsData(newBoardsData);
   };
 
-  // selected boards message
-  const [selectedBoard, setSelectedBoard] = useState();
-  // function to update selected board message
-  const updateSelectedBoard = (selectedBoard) => {
-    setSelectedBoard(selectedBoard);
-    console.log(selectedBoard);
-    //console.log(selectedBoard[0])
+  // state to track selected board id
+  const [selectedBoardID, setSelectedBoardID] = useState(1);
+  // function to update selected board state id
+  const updateSelectedBoard = (selectedBoardElementID) => {
+    setSelectedBoardID(selectedBoardElementID);
   };
 
   return (
@@ -160,7 +228,10 @@ function App() {
         <header>
           <h3>Selected Board</h3>
         </header>
-        <SelectedBoard selectedBoard={selectedBoard}></SelectedBoard>
+        <SelectedBoard
+          selectedBoardID={selectedBoardID}
+          boardsData={boardsData}
+        ></SelectedBoard>
         <header>
           <h3>Create a New Board</h3>
         </header>
@@ -168,7 +239,7 @@ function App() {
       </section>
       <ShowCard />
       <CardList
-        selectedBoard={selectedBoard}
+        selectedBoardID={selectedBoardID}
         cardsData={cardsData}
         likeCard={likeCard}
         deleteCard={deleteCard}
