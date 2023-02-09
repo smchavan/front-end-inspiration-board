1c75149a4ec322b5d382c834e4f9a90d6334019d
annadu <duyuan0921@gmail.com>
2023-02-09 09:08:05 -0800

    put all card function in card list

--- src/App.js
+++ src/App.js
@@ -1,85 +1,15 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import Card from "./components/cards/Card";
-import NewCard from "./components/cards/NewCard";
+
 import BoardsDropDown from "./components/boards/BoardsDropDown";
 import SelectedBoard from "./components/boards/SelectedBoard";
 import NewBoardForm from "./components/boards/NewBoardForm";
-//import CardList from './components/cards/CardList'
+
 import axios from "axios";
 import CardList from "./components/cards/CardList";
 import ShowCard from "./components/cards/ShowCard";
 
 function App() {
-  // cards data
-  const [cardsData, setCardsData] = useState([
-    {
-      board_id: 1,
-      board_title: "Legos are Fun!",
-      id: 1,
-      likes_count: 3,
-      message: "Lego Lego Lego Fun",
-    },
-    {
-      board_id: 1,
-      board_title: "Legos are Fun!",
-      id: 2,
-      likes_count: 4,
-      message: "Puzzles are also Fun like Legos",
-    },
-    {
-      board_id: 1,
-      board_title: "Legos are Fun!",
-      id: 3,
-      likes_count: 4,
-      message: "test delete",
-    },
-  ]);
-
-  const getAllCards = (boardId) => {
-    axios
-      .get(`${process.env.REACT_APP_BACKEND_URL}/boards/${boardId}/cards`)
-      .then((response) => {
-        console.log(response.data);
-        setCardsData(response.data.cards);
-      })
-      .catch((error) => {
-        console.error(error.response.data.message);
-      });
-  };
-
-  const addCard = (message) => {
-    const newCardList = [...cardsData];
-
-    const nextId = Math.max(...newCardList.map((card) => card.id)) + 1;
-
-    newCardList.push({
-      id: nextId,
-      message: message,
-    });
-    setCardsData(newCardList);
-  };
-
-  const likeCard = () => {
-    const cards = cardsData.map((card) => {
-      if (card.id === 2) {
-        card.likes_count += 1;
-        return card;
-      } else {
-        return card;
-      }
-    });
-
-    setCardsData(cards);
-  };
-
-  const deleteCard = () => {
-    const newcards = cardsData.filter((card) => card.id !== 3);
-    console.log(newcards);
-    setCardsData(newcards);
-  };
-
-
   // ~~~~~~ boards data ~~~~~~
   const [boardsData, setBoardsData] = useState([]);
   // function to initialize boards data upon mounting
@@ -112,6 +42,8 @@ function App() {
   // function to update selected board message
   const updateSelectedBoard = (selectedBoard) => {
     setSelectedBoard(selectedBoard);
+    console.log(selectedBoard);
+    //console.log(selectedBoard[0])
   };
 
   return (
@@ -139,11 +71,12 @@ function App() {
       <ShowCard />
       <CardList
         selectedBoard={selectedBoard}
-        cardsData={cardsData}
-        likeCard={likeCard}
-        deleteCard={deleteCard}
+        //cardsData={cardsData}
+        //likeCard={likeCard}
+        //deleteCard={deleteCard}
+        //addCard={addCard}
+        //getBoardCards={getBoardCards}
       />
-      <NewCard addCard={addCard} />
       <footer>
         <h4>Made by Anna, Larissa, Melody, Supriya </h4>
       </footer>
