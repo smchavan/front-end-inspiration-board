d6b515b3f972a8f2b4609467578b5c21e2b0b528
annadu <duyuan0921@gmail.com>
2023-02-09 19:45:44 -0800

    detele return

--- src/components/cards/CardList.js
+++ src/components/cards/CardList.js
@@ -57,26 +57,7 @@ const CardList = (props) => {
     );
   }
   
-  // return (
-  //   <>
-  //     <div>
-  //       {props.cardsData.map((card) => (
-  //         <Card
-  //           key={card.id}
-  //           card_id={card.id}
-  //           board_id={card.board_id}
-  //           likes_count={card.likes_count}
-  //           message={card.message}
-  //           likeCard={props.likeCard}
-  //           deleteCard={props.deleteCard}
-  //         />
-  //       ))}
-  //     </div>
-  //     <div>
-  //       <NewCard addCard={props.addCard} />
-  //     </div>
-  //   </>
-  // );
+  
 };
 
 export default CardList;
